aa4d228970b553abfce734a4b122f5c58ec45ba5
Vedang <vedangrane430@gmail.com>
2023-11-06 12:33:24 +0530

    commit:4

--- myweb/watch/models.py
+++ myweb/watch/models.py
@@ -7,6 +7,10 @@ class Item(models.Model):
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
 
+    def __str__(self):
+        return self.item_name
+
+
 
 
 
--- myweb/watch/views.py
+++ myweb/watch/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from watch.models import Item
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1 style="color:crimson">This is an index view</h1>')
+    itemlist = Item.objects.all()
+    return HttpResponse(itemlist)
 
 def detail(request):
     return HttpResponse('<h1 style="color:blue">This is a detail view</h1>')
