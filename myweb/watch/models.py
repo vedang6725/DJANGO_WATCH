24c1e16a5f4aa983f046dabb927a3a728d393c07
Vedang <vedangrane430@gmail.com>
2023-11-06 12:03:47 +0530

    commit:3

--- myweb/watch/models.py
+++ myweb/watch/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=50)
+    item_desc = models.CharField(max_length=300)
+    item_price = models.IntegerField()
+
+
+
+
+
+
