5ec31588948577433998de77a21914a3575257b2
Vedang <vedangrane430@gmail.com>
2023-11-21 11:26:14 +0530

    commit:12

--- myweb/watch/urls.py
+++ myweb/watch/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from watch import views
 
+app_name = 'watch'
+
 urlpatterns = [
     path('home/', views.index, name='index'),
     path('detail/<int:item_id>/', views.detail, name='detail'),
