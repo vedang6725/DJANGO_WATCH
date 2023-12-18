527b3848ade70ba7ad6b46a6326476349661b524
Vedang <vedangrane430@gmail.com>
2023-12-18 13:13:09 +0530

    commit:27

--- myweb/users/views.py
+++ myweb/users/views.py
@@ -14,10 +14,10 @@ def register(request):
            username = form.cleaned_data.get('username')
         messages.success(
                 request, 
-                'Welcome {}, your account has been successfully created'.format(username)
+                'Welcome {}, your account has been successfully created.Now you may log in'.format(username)
             )
         form.save()
-        return redirect('watch:index')
+        return redirect('login')
 
     else:
         form = RegisterForm()
