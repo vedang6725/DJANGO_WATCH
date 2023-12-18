320c233cf9fcdfa63e6af61d8f60a5873bf109a0
Vedang <vedangrane430@gmail.com>
2023-12-18 12:35:50 +0530

    commit:22

--- myweb/users/views.py
+++ myweb/users/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
 
-    context = {
-        'form':form
-    }
+    if form.is_valid():
+        form.save()
+        return redirect('watch:index')
 
-    return render(request, 'users/register.html', context)
+    else:
+        form = UserCreationForm()
+
+        context = {
+            'form':form
+        }
+
+        return render(request, 'users/register.html', context)
