fbc51a70c01962ec0e4c43515d378592119eec18
Vedang <vedangrane430@gmail.com>
2023-12-19 21:56:05 +0530

    commit:29

--- myweb/users/views.py
+++ myweb/users/views.py
@@ -37,7 +37,22 @@ def login_view(request):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
 
-        if user is not None:
+        if user is None:
+            messages.success(
+                request,
+                'Invalid Login, try again'
+            )
+            return redirect('login')
+
+        elif user.is_superuser:
+            login(request, user)
+            messages.success(
+                request,
+                'Welcome Superuser {}, you have been successfully logged in'.format(request.user.username)
+            )
+            return redirect('food:index')
+
+        elif user is not None:
             login(request, user)
             messages.success(
                 request,
