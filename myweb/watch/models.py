c8322cdd29bb6291a2fd183fcfc17725e68294c0
Vedang <vedangrane430@gmail.com>
2023-12-20 13:33:57 +0530

    commit:32

--- myweb/watch/models.py
+++ myweb/watch/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Item(models.Model):
+    CATEGORY_CHOICES = (
+        ('m', 'Mens'),
+        ('f', 'Womens'),
+        ('k', 'Kids'),
+    )
+
+    category = models.CharField(max_length=1, choices=CATEGORY_CHOICES, default="('m','Mens')")
     prod_code = models.IntegerField(default=100)
     for_user = models.CharField(
         max_length=100, 
