3227e5914568393149736b66dacd5e44972d9ec4
Vedang <vedangrane430@gmail.com>
2023-12-22 09:59:33 +0530

    commit:35

--- myweb/users/forms.py
+++ myweb/users/forms.py
@@ -14,4 +14,4 @@ class RegisterForm(UserCreationForm):
 class CusOrdersUpd(forms.ModelForm):
     class Meta:
         model = CusOrders
-        fields = ['order_id', 'prod_code', 'quantity', 'user']
+        fields = ['quantity']
--- myweb/users/views.py
+++ myweb/users/views.py
@@ -106,7 +106,15 @@ def update_orders(request, id, upd_order_id):
         'form':form
     }
 
-    if form.is_valid():
+    if request.method == 'POST':
+        form.instance.order_id = coo.order_id
+        form.instance.prod_code = coo.prod_code
+        form.instance.user = request.user.username
+        
+        
+        
+        
+        
         form.save()
         return redirect('watch:detail', item_id=id)
 
